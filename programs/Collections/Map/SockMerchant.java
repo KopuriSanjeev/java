7c27467937577e13034657c7e23dba1e7c2dce33
KopuriSanjeev <35334095+KopuriSanjeev@users.noreply.github.com>
2020-09-10 12:03:57 +0530

    Create SockMerchant.java

---
+++ programs/Collections/Map/SockMerchant.java
@@ -0,0 +1,45 @@
+package test;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+   /*
+    This program is taken from
+    https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+   */
+	public static void main(String[] args) {
+	
+		int[] ar = new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20};
+		Map<Integer,Integer> m = new HashMap<Integer,Integer>();
+		int count = 0;
+		int pairs = 0;
+		for(int i: ar)
+		{
+			if(m.containsKey(i))
+			{
+				count = m.get(i);
+				m.put(i, ++count);
+			}
+			else
+				m.put(i, 1);
+		}
+		//System.out.println(m);
+		Set<Integer> keys = m.keySet();
+		for(Integer key : keys)
+		{
+			int value = m.get(key);
+			pairs += value/2;
+		}
+		System.out.println(pairs);
+	
+	}
+
+}
