8b342894a150367931326a7a30e9d750ad45df0a
KopuriSanjeev <35334095+KopuriSanjeev@users.noreply.github.com>
2021-06-12 13:46:03 +0530

    Create Program1.java

---
+++ interview_preparation/tricky_program/Program1.java
@@ -0,0 +1,29 @@
+class Program1
+{
+	public static void main(String[] args)
+	{
+		// the line below gives an output
+		
+		//\u000d System.out.println("Comment executed");
+	
+	}
+}
+
+
+/*
+The output is 
+Comment executed
+
+Explanation:
+\u000d is the unicode for new line. So the program will be like
+
+public static void main(String[] args)
+	{
+		// the line below gives an output
+		
+		//
+    System.out.println("Comment executed");
+	
+	}
+
+*/
